299ce7e6170795d97084940f465296116422a704
Andoni <a.ventura@upm.es>
2020-07-08 02:13:31 +0200

    a

--- afs/ViceWriter.java
+++ afs/ViceWriter.java
@@ -5,6 +5,7 @@ import java.rmi.*;
 import java.io.IOException;
 
 public interface ViceWriter extends Remote {
+    public void setLength(long l) throws RemoteException, IOException;
     public void write(byte [] b) throws RemoteException, IOException;
     public void close() throws RemoteException, IOException ;
     /* añada los métodos remotos que requiera */
